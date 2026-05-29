a2f8ed7892a4fad28ef2d83e414ef7ed639e0053
unknown <varshinieswaramoorthi@gmail.com>
2026-05-29 19:30:20 +0530

    Iris Flower Classification Project

---
+++ iris_model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load Dataset
+df = pd.read_csv("Iris.csv")
+
+# Features
+X = df[['SepalLengthCm',
+        'SepalWidthCm',
+        'PetalLengthCm',
+        'PetalWidthCm']]
+
+# Target
+y = df['Species']
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train Model
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Evaluate
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
